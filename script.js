13d0c2296ea9bddc7da11e790f37da63fdea6223
Williams Adele <williams.adele.dev@gmail.com>
2026-03-30 14:15:25 +0100

    finished basic people counter

--- script.js
+++ script.js
@@ -55,3 +55,9 @@ function increment() {
     count = count + 1
     countEl.innerText = count
 }
+
+function decrement() {
+    count = count - 1
+    countEl.innerText = count  
+}
+console.log(count)
