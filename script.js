88987c279677df72f48e92ac62fadafdd478a5c4
Williams Adele <williams.adele.dev@gmail.com>
2026-04-05 14:24:48 +0100

    created basic calculator app

--- script.js
+++ script.js
@@ -1,99 +1,29 @@
-//document.getElementById("count").innerText = 5
-// let myAge = 21 - 1
+let num1 = 1
+let num2 = 5
 
-// console.log(myAge)
-// let myAge = 21
-// let humanDogRatio = 7
-// let myDogAge = myAge * humanDogRatio
+let num1El = document.getElementById("num1-el")
+num1El.textContent = num1
 
-// console.log(myDogAge)
+let num2El = document.getElementById("num2-el")
+num2El.textContent = num2
 
-// let bonusPoints = 50
-
-// bonusPoints = bonusPoints + 50
-// bonusPoints = bonusPoints - 75
-// bonusPoints = bonusPoints + 45
-// console.log(bonusPoints)
+let resultEl = document.getElementById("result-el")
 
 
-
-// function increament() {
-//     console.log("The button was clicked")
-// }
-
-// function racecar() {
-//     console.log(42)
-// }
-// racecar()
-
-// let lap1 = 34
-// let lap2 = 33
-// let lap3 = 36
-
-// function logLapTime() {
-//     let totalTime = lap1 + lap2 + lap3
-//     console.log(totalTime)
-// }
-// logLapTime()
-
-// let lapsCompleted = 0
-
-// function increamentLap() {
-//     lapsCompleted = lapsCompleted + 1
-// }
-
-// increamentLap()
-// increamentLap()
-// increamentLap()
-
-// console.log(lapsCompleted)
-
- let countEl = document.getElementById("count-el")
- let saveEl = document.getElementById("save-el")
-console.log(countEl)
-let count = 0
-function increment() {
-    count = count + 1
-    countEl.textContent = count
+function add(){
+    let result = num1 + num2
+    resultEl.textContent = "Answer: " + result
 }
-
- function decrement() {
-     count = count - 1
-     countEl.textContent = count  
- }
- console.log(count)
-
- function save() {
-     let countStr = count + " - "
-     saveEl.textContent += countStr
-     console.log(count)
- }
- function reset() {
-    count = 0
-    countEl.textContent = count
- }
-
-// let message = "You have three new notifications"
-
-// console.log(message)
-
-// let messageTouser = "we have logged"
-
-
-// let meme = "you out"
-
-
-// let message = messageTouser + ", " + meme
-
-// console.log(message)
-
-// let myname = "will"
-// let greeting = "hi, my name is"
-// let myGreeting = greeting + " " + myname
-// console.log(myGreeting)
-// let welcomeEl = document.getElementById("welcome-el")
-// let myname = "Will"
-// let greeting = "Welcome back"
-
-// welcomeEl.innerText = greeting + ", " + myname
-// console.log(welcomeEl)
+function subtract(){
+    let result = num1 - num2
+    resultEl.textContent = "Answer: " + result
+}
+function multiply(){
+    let result = num1 * num2
+    resultEl.textContent = "Answer: " + result
+}
+function divide(){
+    let result = num1 / num2
+    resultEl.textContent = "Answer: " + result
+}
+console.log(resultEl)
