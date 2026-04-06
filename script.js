768e7af8dfdcb99fc8486ef49806d072ec3ce0dd
Williams Adele <williams.adele.dev@gmail.com>
2026-04-06 12:35:21 +0100

    if else

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-let firstCard = 10
-let secondCard = 11
-
-let sum = firstCard + secondCard
-console.log(sum)
-
-if (sum < 21) {
-    console.log("Do you want to draw a new card?")
+let age = 22
+age = 22 - 1
+if (age < 22) {
+    console.log("You are not old enough to enter")
 }
-else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack!")
-}
-else  {
-    console.log("You're out of the game!")
+
+else {
+    console.log("Welcome!")
 }
