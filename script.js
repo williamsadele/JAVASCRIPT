1d36d200d18f93b9af1cb7369a8e7ec7c15a856c
Williams Adele <williams.adele.dev@gmail.com>
2026-04-10 12:46:55 +0100

    more learning, intro to arrays

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let firstCard = 10
 let secondCard = 11
 let sum = firstCard + secondCard
+let cards = [firstCard, secondCard]
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
@@ -10,6 +11,9 @@ let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
 function startGame() {
+    renderGame()
+}
+function renderGame() {
     if (sum <= 20) {
     message = "Do you want to draw a new card?"
 }
@@ -24,5 +28,11 @@ else{
 }
 messageEl.textContent = message
 sumEl.textContent = "Sum: " + sum
-cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard
+cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1]
+}
+
+function newCard() {
+    let card = 5
+    sum += card
+    renderGame()
 }
