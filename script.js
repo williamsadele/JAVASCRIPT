1fca7801edfa56038b68e2915f55a5b76d5cf6de
Williams Adele <williams.adele.dev@gmail.com>
2026-04-11 14:06:24 +0100

    arrays and some methods

--- script.js
+++ script.js
@@ -1,38 +1,18 @@
+let posts = [
+    "Check out my new website",
+    "Do you like it",
+    "what do you think about it"
+]
+posts.push("Hey")
+console.log(posts)
 
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
-let cards = [firstCard, secondCard]
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-
-let sumEl = document.querySelector("#sum-el")
-let cardsEl = document.querySelector("#cards-el")
-
-function startGame() {
-    renderGame()
-}
-function renderGame() {
-    if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-}
-
-else if (sum === 21) {
-    message = "Woohoo!"
-}
-
-else{
-    message = "You loose"
-    isAlive = false
-}
-messageEl.textContent = message
-sumEl.textContent = "Sum: " + sum
-cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1]
-}
-
-function newCard() {
-    let card = 5
-    sum += card
-    renderGame()
-}
+let experience = [
+    "Procurement",
+    "Secretary",
+    "Developer",
+    50
+]
+let newExperience = "Editor"
+experience.push(newExperience)
+experience.splice(4, 1)
+console.log(experience)
