8b09b8018e6540639fe62483615302ab2a72a56a
Williams Adele <williams.adele.dev@gmail.com>
2026-04-13 14:24:19 +0100

    using array methods

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-let messages = [
-    "Hello, World!",
-    "Welcome to JavaScript programming.",
-    "Let's learn together!",
-    "What's the lesson of the day?",
-    "Hello"
+let cards = [6,9,5]
+
+for (let i = 0; i < cards.length; i += 1)  [
+    console.log(cards[i])
 ]
-
-
-
-for( let i = 0; i < messages.length; i += 1) {
-    console.log(messages[i])
-}
