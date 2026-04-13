4dd404b4a635002772045e20cef522c09dcdce5b
Williams Adele <williams.adele.dev@gmail.com>
2026-04-13 14:18:15 +0100

    using loops with arrays

--- script.js
+++ script.js
@@ -1,38 +1,13 @@
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
-let cards = [firstCard, secondCard]
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
+let messages = [
+    "Hello, World!",
+    "Welcome to JavaScript programming.",
+    "Let's learn together!",
+    "What's the lesson of the day?",
+    "Hello"
+]
 
-let sumEl = document.querySelector("#sum-el")
-let cardsEl = document.querySelector("#cards-el")
 
-function startGame() {
-    renderGame()
-}
-function renderGame() {
-    if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-}
 
-else if (sum === 21) {
-    message = "Woohoo!"
-}
-
-else{
-    message = "You loose"
-    isAlive = false
-}
-messageEl.textContent = message
-sumEl.textContent = "Sum: " + sum
-cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1]
-}
-
-function newCard() {
-    let card = 5
-    sum += card
-    cards.push(card)
-    renderGame()
+for( let i = 0; i < messages.length; i += 1) {
+    console.log(messages[i])
 }
