dd903dcb91e260129d9000922341888b0b7964cb
Williams Adele <williams.adele.dev@gmail.com>
2026-06-10 13:48:17 +0100

    Used logic AND operator to control when to give new card(line 51)

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
-let likesDocumentaries = true;
-let likesStartups = true;
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let sum = firstCard + secondCard
+let cards = [firstCard, secondCard]
+let isAlive = true
+let hasBlackJack = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-if(likesDocumentaries === true || likesStartups === true) {
-    recommendMovie()
+
+function startGame() {
+    renderGame()
 }
-function recommendMovie() {
-    console.log("You should watch 'The Social Dilemma'!");
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    }
+    else if (randomNumber === 1) {
+        return 11
+    }
+    else {
+        return randomNumber
+    }
+}
+function renderGame() {
+    if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+}
+
+else if (sum === 21) {
+    message = "Woohoo!"
+    hasBlackJack = true
+}
+
+else{
+    message = "You loose"
+    isAlive = false
+}
+messageEl.textContent = message
+sumEl.textContent = "Sum: " + sum
+cardsEl.textContent = "Cards: "
+for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " "
+}
+}
+
+function newCard() {
+    if(isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+    }
 }
