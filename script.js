1b1346bb80565c634db048209e9c6ceb80544c2d
Williams Adele <williams.adele.dev@gmail.com>
2026-06-10 13:10:04 +0100

    Logic operators

--- script.js
+++ script.js
@@ -1,53 +1,9 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let sum = firstCard + secondCard
-let cards = [firstCard, secondCard]
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let hasSolvedChallenge = false;
+let hasHintsLeft = false;
 
-
-function startGame() {
-    renderGame()
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+    showSolution();
 }
-function getRandomCard() {
-    let randomNumber = Math.floor(Math.random() * 13) + 1
-    if (randomNumber > 10) {
-        return 10
-    }
-    else if (randomNumber === 1) {
-        return 11
-    }
-    else {
-        return randomNumber
-    }
-}
-function renderGame() {
-    if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-}
-
-else if (sum === 21) {
-    message = "Woohoo!"
-}
-
-else{
-    message = "You loose"
-    isAlive = false
-}
-messageEl.textContent = message
-sumEl.textContent = "Sum: " + sum
-cardsEl.textContent = "Cards: "
-for (let i = 0; i < cards.length; i++) {
-    cardsEl.textContent += cards[i] + " "
-}
-}
-
-function newCard() {
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    renderGame()
+function showSolution() {
+    console.log("Showing the solution...");
 }
