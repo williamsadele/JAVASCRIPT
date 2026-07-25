0325ee2813cc648150d47ae4fabaebb4484d70e1
Williams Adele <williams.adele.dev@gmail.com>
2026-07-25 19:00:45 +0100

    same as last commit

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-let myLeads = []
-const inputEl =document.getElementById("input-el")
-const inputBtn =document.getElementById("input-btn")
-=======
+
 let myLeads = ["www.google.com", "www.youtube.com", "www.facebook.com"]
 const inputEl = document.getElementById("input-el")
 const openBox = document.getElementById("box")
@@ -15,4 +11,3 @@ openBox.addEventListener("click", function(){
 for (let i = 0; i < myLeads.length; i++) {
     console.log(myLeads[i])
 }
->>>>>>> 2dc0cf8 (Working with event listeners, for loops and arrays)
