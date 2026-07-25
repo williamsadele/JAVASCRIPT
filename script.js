d4773de9f9001d0fc7d7183e3d23b4d0c356ac17
Williams Adele <williams.adele.dev@gmail.com>
2026-07-25 15:17:10 +0100

    loops, arrays an event listeners

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
 let myLeads = []
 const inputEl =document.getElementById("input-el")
 const inputBtn =document.getElementById("input-btn")
+=======
+let myLeads = ["www.google.com", "www.youtube.com", "www.facebook.com"]
+const inputEl = document.getElementById("input-el")
+const openBox = document.getElementById("box")
+
+openBox.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}
+>>>>>>> 2dc0cf8 (Working with event listeners, for loops and arrays)
