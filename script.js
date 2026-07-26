b02e85be36683f8637685c1723cf351e1d172cd1
Williams Adele <williams.adele.dev@gmail.com>
2026-07-26 19:41:26 +0100

    created ul with js

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 let myLeads = ["www.google.com", "www.youtube.com", "www.facebook.com"]
 const inputEl = document.getElementById("input-el")
 const openBox = document.getElementById("box")
+const ulEl = document.getElementById("ul-el")
 
 openBox.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 
+console.log(ulEl)
 for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
+    ulEl.textContent += myLeads[i] + " "
 }
