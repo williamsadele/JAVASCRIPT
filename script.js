dbb29ece2c61d938cb3a251409c88eaacfd54ec1
Williams Adele <williams.adele.dev@gmail.com>
2026-08-02 17:59:50 +0100

    inner.HTML practice

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-// let myLeads = ["www.google.com", "www.youtube.com", "www.facebook.com"]
-// const inputEl = document.getElementById("input-el")
-// const openBox = document.getElementById("box")
-// const ulEl = document.getElementById("ul-el")
+let myLeads = ["www.google.com", "www.youtube.com", "www.facebook.com"]
+const inputEl = document.getElementById("input-el")
+const openBox = document.getElementById("box")
+const ulEl = document.getElementById("ul-el")
 
-// openBox.addEventListener("click", function(){
-//     myLeads.push(inputEl.value)
-//     ulEl.textContent = ""
-//     for (let i = 0; i < myLeads.length; i++) {
-//         ulEl.textContent += myLeads[i] + " "
-//     }
-// })
-const container = document.getElementById("container")
-container.innerHTML = "<button>Click Me</button>"
-container.addEventListener("click", function(){
-    container.innerHTML += "<p>THANK YOU!</p>"
+openBox.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    ulEl.innerHTML = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    }
 })
+// const container = document.getElementById("container")
+// container.innerHTML = "<button>Click Me</button>"
+// container.addEventListener("click", function(){
+//     container.innerHTML += "<p>THANK YOU!</p>"
+// })
