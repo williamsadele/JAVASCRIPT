21a6608b8460e71be7f876b34a4011491a8f5f2f
Williams Adele <williams.adele.dev@gmail.com>
2026-08-05 09:06:12 +0100

    render list to DOM outside the loop & created function that iterates loop whenever button is clicked

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 
-let myLeads = ["www.google.com", "www.youtube.com", "www.facebook.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const openBox = document.getElementById("box")
 const ulEl = document.getElementById("ul-el")
 
 openBox.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    ulEl.innerHTML = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    }
+    renderLeads()
 })
-const container = document.getElementById("container")
-container.innerHTML = "<button>Click Me</button>"
-container.addEventListener("click", function(){
-    container.innerHTML += "<p>THANK YOU!</p>"
-})
-const li = document.createElement("li")
-li.textContent = myLeads[i]
-ulEl.append(li)
-
-li.textContent = myLeads[i]
-ulEl.append(li)
+function renderLeads() {
+ let listItems = ""
+ for (let i = 0; i < myLeads.length; i++) {
+     listItems += "<li>" + myLeads[i] + "</li>"
+ }
+ ulEl.innerHTML = listItems
+}
