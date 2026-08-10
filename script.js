0a934e66cceba6e858d867ab58a1d6d1214d5f86
Williams Adele <williams.adele.dev@gmail.com>
2026-08-10 12:49:13 +0100

    Chrome extension final commit

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ const ulEl = document.getElementById("ul-el")
 
 openBox.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     renderLeads()
 })
 function renderLeads() {
  let listItems = ""
  for (let i = 0; i < myLeads.length; i++) {
-     listItems += "<li>" + myLeads[i] + "</li>"
+     listItems += `
+     <li>
+        <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}
+        </a>
+     </li>
+     `
  }
  ulEl.innerHTML = listItems
 }
